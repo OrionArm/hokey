9d12323b01b8d8bd2b15467f92a00d4912b1dfe1
M.Roman <moshchenkov.r@cronix.ms>
2018-09-19 18:05:55 +0300

    fix(headerLogo): revert file

--- src/layout/logoListPage/headerLogo.tsx
+++ src/layout/logoListPage/headerLogo.tsx
@@ -1,75 +1,21 @@
-import React, { Component } from 'react';
-import {
-  Typography,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-} from '@material-ui/core';
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
 
 type Props = { addLogo(): void };
-class HeaderLogo extends Component<Props> {
-  state = {
-    open: false,
-  };
+const HeaderLogo: React.SFC<Props> = ({ addLogo }) => {
+  const onClick = () => addLogo();
+  return (
+    <>
+      <Typography variant="headline">My Logos</Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={onClick}
+      >
+        new logos
+      </Button>
+    </>
+  );
+};
 
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  }
-
-  handleClose = () => {
-    this.setState({ open: false });
-  }
-
-  render() {
-    return (
-      <>
-        <Typography variant="headline">My Logos</Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={this.handleClickOpen}
-        >
-          new logos
-        </Button>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {"Use Google's location service?"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            variant="contained"
-            <Button
-              onClick={this.handleClose}
-              variant="contained"
-              color="primary"
-            >
-              Upload
-            </Button>
-            <Button
-              onClick={this.handleClose}
-              variant="contained"
-              color="primary"
-              autoFocus
-            >
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </>
-    );
-  }
-}
 export default HeaderLogo;
