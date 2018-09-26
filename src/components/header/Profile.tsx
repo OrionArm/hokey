19e91fb815f9934f754bb9b5d552cf4323506813
M.Roman <moshchenkov.r@cronix.ms>
2018-09-26 12:15:59 +0300

    fix: logout

--- src/components/header/Profile.tsx
+++ src/components/header/Profile.tsx
@@ -1,14 +1,43 @@
-import * as React from 'react';
+import React, { SFC } from 'react';
 import { withStyles } from '@material-ui/core';
 import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { Link } from 'react-router-dom';
-import { RootState } from 'src/store/rootReducers';
+import { compose, Dispatch } from 'redux';
 
-export interface Props {
-  classes?: any;
-  userName: string;
-}
+import { RootState } from 'src/store/rootReducers';
+import userActions from 'src/store/user/store/actions';
+
+const mapStateToProps    = (state: RootState) => ({
+  userName: state.user.profile ? state.user.profile.last : '',
+});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  logOut: () => dispatch(userActions.logoutRequest()),
+});
+
+type Props = { classes?: any } & injectDispatchProps & injectStateProps;
+const Profile: SFC<Props> = ({ userName, logOut, classes }) => {
+  const onLogOut = () => logOut();
+  if (userName) {
+    return (
+      <div className={'app-toolbar__info'}>
+        <h3 className={'app-toolbar__username'}>
+          {userName}
+        </h3>
+        <div
+          className={'app-toolbar__action'}
+          onClick={onLogOut}
+        >
+          <p
+            className={`${classes.textLogout} app-toolbar__link`}
+            // className={'app-toolbar__link'}
+          >
+            (log out)
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
 
 const styles = (theme: any) => ({
   wrapperProfile: {
@@ -17,7 +46,7 @@ const styles = (theme: any) => ({
   },
 
   textLogout: {
-    color: theme.palette.primary.contrastText,
+    color: theme.palette.text.secondary,
     borderBottom: `1px solid ${theme.palette.primary.contrastText}`,
     textDecoration: 'none',
     transition: '0.7s',
@@ -35,31 +64,11 @@ const styles = (theme: any) => ({
     },
   },
 });
-class Profile extends React.Component<Props, any> {
-  public render() {
-    const { userName } = this.props;
 
-    return (
-      <div className={'app-toolbar__info'}>
-        <h3 className={'app-toolbar__username'}>
-          {userName}
-        </h3>
-        <div className={'app-toolbar__action'}>
-          (<Link to="/login" className={'app-toolbar__link'}>
-          log out
-        </Link>)
-        </div>
-
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: RootState) => ({
-  userName: state.user.profile ? state.user.profile.last : '',
-});
+type injectDispatchProps = ReturnType<typeof mapDispatchToProps>;
+type injectStateProps = ReturnType<typeof mapStateToProps>;
 
 export default compose(
   withStyles(styles),
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
 )(Profile);
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -6,10 +6,11 @@ export default createMuiTheme({
     fontFamily: ['Poppins', 'sans-serif'].join(','),
     fontSize: 14,
   },
-
+// #4F5467
   palette: {
     primary: { main: '#FB9678' },
     secondary: { main: '#3AB7F1' },
+    text: { primary: '#4d4d4d', secondary: '#a7a9b3' },
     action: {
       // selected: 'rgb(251, 171, 147)',
       // hover: 'rgb(251, 171, 147)',
